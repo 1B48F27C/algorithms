15f97e7cfad02fa42d09c525a4ddbc20dea4ee8e
IvanM <ivan.mkhitaryan@adacta-group.com>
2018-08-03 10:47:55 +0300

    format code

--- algorithms/sortings.js
+++ algorithms/sortings.js
@@ -1,15 +1,14 @@
 let utils = require('./utils');
 
-module.exports = (function () 
-{
-    let bubbleSort = function(arr){
-        if (arr && arr.length > 1){
+module.exports = (function () {
+    let bubbleSort = function (arr) {
+        if (arr && arr.length > 1) {
             let swapped = true;
             while (swapped) {
                 swapped = false;
-                for (let i = 0; i<arr.length-1; i++) {
-                    if (arr[i] > arr[i+1]){
-                        arr = utils.swap(arr, i, i+1);
+                for (let i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        arr = utils.swap(arr, i, i + 1);
                         swapped = true;
                     }
                 }
@@ -18,17 +17,16 @@ module.exports = (function ()
         return arr
     }
 
-    let selectionSort = function(arr){
-        if (arr && arr.length > 1){
+    let selectionSort = function (arr) {
+        if (arr && arr.length > 1) {
             let minIndex;
-            for (let i=0; i<arr.length-1; i++) {
-                for (let j = i+1; j<arr.length; j++)
-                {
-                    if (arr[j]<arr[i]) 
-                    arr = utils.swap(arr, i, j)
+            for (let i = 0; i < arr.length - 1; i++) {
+                for (let j = i + 1; j < arr.length; j++) {
+                    if (arr[j] < arr[i])
+                        arr = utils.swap(arr, i, j)
                 }
             }
-        }        
+        }
         return arr
     }
 
